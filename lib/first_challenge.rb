a5807c3c0e2e3c3fe8cd019840f5962ae6b18c31
Michelle M <michellemoon236@gmail.com>
2019-03-23 19:42:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,11 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |person, data|
+  
+  data.each do |attribute, value|
+    
+  if attribute ==
 
   #remember to return your newly altered contacts hash!
   contacts
